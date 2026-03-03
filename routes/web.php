fd598091a6b83b4c5a94d936f9e67415d5c31cea
Muhammad-Umar123 <m.umar@conviersolutions.com>
2026-03-03 16:57:17 +0500

    Remove home view and stub root route

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [DashboardController::class, 'home'])->name('home');
+Route::get('/', function () {
+    return ;
+})->name('home');
 
 
 Route::middleware('guest')->group(function () {
